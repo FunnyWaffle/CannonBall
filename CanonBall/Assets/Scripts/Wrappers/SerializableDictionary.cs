40db1e6eb9ee7818ba0772bff013c968cb30ece2
Artem <nadulich2019@mail.ru>
2026-04-07 19:02:07 +0300

    added serializable dictionary

---
+++ CanonBall/Assets/Scripts/Wrappers/SerializableDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Wrappers
+{
+    [Serializable]
+    public class SerializableDictionary<TKey, TValue> : ISerializationCallbackReceiver
+    {
+        private readonly Dictionary<TKey, TValue> _dictionary = new();
+
+        [SerializeField] private List<SerializableKeyValuePair<TKey, TValue>> _list = new();
+
+        public Dictionary<TKey, TValue> Dictionary => _dictionary;
+
+        public void OnAfterDeserialize()
+        {
+            _dictionary.Clear();
+            foreach (var keyValuePair in _list)
+            {
+                if (keyValuePair.Key == null)
+                    continue;
+
+                _dictionary[keyValuePair.Key] = keyValuePair.Value;
+            }
+        }
+
+        public void OnBeforeSerialize()
+        {
+            _list.Clear();
+            foreach (var (key, value) in _dictionary)
+            {
+                _list.Add(new SerializableKeyValuePair<TKey, TValue>(key, value));
+            }
+        }
+    }
+}
