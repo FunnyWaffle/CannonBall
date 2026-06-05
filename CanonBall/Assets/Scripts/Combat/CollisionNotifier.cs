d1c09ccc6fa0712e13bb402cc667535ab4ee7984
Artem <nadulich2019@mail.ru>
2026-06-05 11:29:28 +0300

    Added CollisionNotifier

---
+++ CanonBall/Assets/Scripts/Combat/CollisionNotifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Combat
+{
+    public class CollisionNotifier : MonoBehaviour
+    {
+        public event Action<Collider> TriggerEntered;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            TriggerEntered?.Invoke(other);
+        }
+    }
+}
