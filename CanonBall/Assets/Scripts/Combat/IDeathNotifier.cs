c532e5c56e92cc97588381052252fab84e20fbc9
Artem <nadulich2019@mail.ru>
2026-06-13 22:33:01 +0300

    added death notifier

---
+++ CanonBall/Assets/Scripts/Combat/IDeathNotifier.cs
@@ -0,0 +1,10 @@
+﻿using Assets.Scripts.Creations;
+using System;
+
+namespace Assets.Scripts.Combat
+{
+    public interface IDeathNotifier : IComponent
+    {
+        public event Action Died;
+    }
+}
