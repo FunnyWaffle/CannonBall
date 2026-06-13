739f461b920e1082687c7b20f464ce6f465e350e
Artem <nadulich2019@mail.ru>
2026-06-13 22:36:16 +0300

    damage system changes

--- CanonBall/Assets/Scripts/Combat/DamageSystem.cs
+++ CanonBall/Assets/Scripts/Combat/DamageSystem.cs
@@ -1,36 +1,26 @@
 ﻿using Assets.Scripts.Creations;
-using System.Collections.Generic;
+using Assets.Scripts.Space;
 using UnityEngine;
 
 namespace Assets.Scripts.Combat
 {
     public class DamageSystem
     {
-        private readonly Dictionary<Collider, IDamageable> _damageables = new();
+        private readonly World _world;
+
+        public DamageSystem(World world)
+        {
+            _world = world;
+        }
 
         public void DealDamage(Collider target, float value)
         {
-            if (!_damageables.TryGetValue(target, out var damageable))
+            if (!_world.SpatialObjectsMap.TryGetValue(target, out var spatialObject)
+                || !_world.EntityComponents.TryGetValue(spatialObject, out var components)
+                || !components.TryGet<IDamageable>(out var damageable))
                 return;
 
             damageable.TakeDamage(value);
-        }
-
-        public void Register(Collider collider, IDamageable damageable)
-        {
-            _damageables[collider] = damageable;
-        }
-        public void Register(IEnumerable<Collider> colliders, IDamageable damageable)
-        {
-            foreach (var collider in colliders)
-            {
-                _damageables[collider] = damageable;
-            }
-        }
-
-        public void Unregister(Collider collider)
-        {
-            _damageables.Remove(collider);
         }
     }
 }
