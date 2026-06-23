b9ea5347befc0e449ea06501430865c0247e6d2b
Artem <nadulich2019@mail.ru>
2026-06-23 18:44:10 +0300

    added Universal Factory inteface

---
+++ CanonBall/Assets/Scripts/Spawn/Factories/IUniversalFactory.cs
@@ -0,0 +1,13 @@
+﻿using Assets.Scripts.Creations;
+using Assets.Scripts.Shop;
+using UnityEngine;
+
+namespace Assets.Scripts.Spawn.Factories
+{
+    public interface IUniversalFactory
+    {
+        public ItemTypes CreationType { get; }
+
+        public EntityComponents Create(Transform prefab, Vector3 position, Quaternion rotation, Transform parent = null);
+    }
+}
