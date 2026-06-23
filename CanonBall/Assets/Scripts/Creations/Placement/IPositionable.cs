a30b6bf5e40a98b5f39be109b0e26d6af9fff89d
Artem <nadulich2019@mail.ru>
2026-06-23 19:15:21 +0300

    added IPositionable

---
+++ CanonBall/Assets/Scripts/Creations/Placement/IPositionable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Creations.Placement
+{
+    public interface IPositionable : IComponent
+    {
+        public void SetPosition(Vector3 position);
+    }
+}
