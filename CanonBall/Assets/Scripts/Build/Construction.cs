970083d06fd62fcbcbc6c2ec08f38d4ea55ae0ff
Artem <nadulich2019@mail.ru>
2026-06-23 19:00:05 +0300

    added Construction

---
+++ CanonBall/Assets/Scripts/Build/Construction.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts.Creations;
+using Assets.Scripts.Creations.Placement;
+using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Build
+{
+    public class Construction : MonoBehaviour, IHasPosition, IComponent
+    {
+        [SerializeField] private Transform[] _parts;
+
+        private int _currentPartIndex = 0;
+
+        public bool Complete => _currentPartIndex == _parts.Length;
+        public int PartCount => _parts.Length;
+
+        public Vector3 Position => transform.position;
+
+        public event Action PartAdded;
+
+        public void BuildNextPart()
+        {
+            var part = _parts[_currentPartIndex];
+            part.gameObject.SetActive(true);
+            _currentPartIndex++;
+
+            PartAdded?.Invoke();
+        }
+    }
+}
