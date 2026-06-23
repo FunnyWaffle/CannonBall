2f2113f69c07434a6e42eb70fa2ec8de47180ce5
Artem <nadulich2019@mail.ru>
2026-06-23 19:27:20 +0300

    added Build Carriage Factory

---
+++ CanonBall/Assets/Scripts/Spawn/Factories/BuildCarriageFactory.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Scripts.Build.Carriage;
+using Assets.Scripts.Creations;
+using Assets.Scripts.Shop;
+using UnityEngine;
+
+namespace Assets.Scripts.Spawn.Factories
+{
+    public class BuildCarriageFactory : IUniversalFactory
+    {
+        public ItemTypes CreationType => ItemTypes.BuildCarriage;
+
+        public EntityComponents Create(Transform prefab, Vector3 position, Quaternion rotation, Transform parent = null)
+        {
+            var obj = GameObject.Instantiate(prefab, position, rotation, parent);
+            var view = obj.GetComponent<BuildCarriage>();
+
+            var components = new EntityComponents();
+            components.Add(view);
+
+            return components;
+        }
+    }
+}
