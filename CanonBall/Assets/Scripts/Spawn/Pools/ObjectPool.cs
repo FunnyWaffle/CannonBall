4c2d7350c0a40e35bcdc46c6ccdbb42a0b8dfb5a
Artem <nadulich2019@mail.ru>
2026-06-23 19:32:16 +0300

    change ObjectPool directory

--- CanonBall/Assets/Scripts/Spawn/Pools/ObjectPool.cs
+++ CanonBall/Assets/Scripts/Spawn/Pools/ObjectPool.cs
@@ -1,7 +1,7 @@
 ﻿using Assets.Scripts.Shop;
 using System.Collections.Generic;
 
-namespace Assets.Scripts.Spawn
+namespace Assets.Scripts.Spawn.Pools
 {
     public class ObjectPool<T>
                 where T : IPoolableObject
