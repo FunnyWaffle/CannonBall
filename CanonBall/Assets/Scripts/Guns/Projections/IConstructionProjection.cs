f8b9b117290a34c689e9af4d58ff4cf800a71dc8
Artem <nadulich2019@mail.ru>
2026-06-23 19:22:52 +0300

    added IConstruction Projection

---
+++ CanonBall/Assets/Scripts/Guns/Projections/IConstructionProjection.cs
@@ -0,0 +1,9 @@
+﻿using Assets.Scripts.Creations;
+
+namespace Assets.Scripts.Guns.Projections
+{
+    public interface IConstructionProjection : IComponent
+    {
+        public float BuildCarriageOffset { get; }
+    }
+}
