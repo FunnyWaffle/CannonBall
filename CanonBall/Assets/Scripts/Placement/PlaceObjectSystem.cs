5283a0258697c6e7447f3d1f495cbd5fb042cd0f
Artem <nadulich2019@mail.ru>
2026-06-23 21:06:07 +0300

    refactor: added BuildSystem workflow in PlaceObjectSystem

--- CanonBall/Assets/Scripts/Placement/PlaceObjectSystem.cs
+++ CanonBall/Assets/Scripts/Placement/PlaceObjectSystem.cs
@@ -1,7 +1,8 @@
-﻿using Assets.Scripts.Config;
+﻿using Assets.Scripts.Build;
+using Assets.Scripts.Config;
 using Assets.Scripts.Creations;
+using Assets.Scripts.Creations.Placement;
 using Assets.Scripts.GameStateMachine;
-using Assets.Scripts.Guns;
 using Assets.Scripts.Guns.Projections;
 using Assets.Scripts.Input;
 using Assets.Scripts.Shop;
@@ -9,11 +10,12 @@ using Assets.Scripts.Spawn;
 using Assets.Scripts.Systems;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Assets.Scripts.Placement
 {
-    public class PlaceObjectSystem : IUpdatable, ISpawnRequester<CannonProjection>, ISpawnRequester<CannonController>
+    public class PlaceObjectSystem : IUpdatable
     {
         private readonly Dictionary<ItemTypes, ItemTypes> _projections = new()
         {
@@ -21,22 +23,30 @@ namespace Assets.Scripts.Placement
         };
         private readonly List<IPlacementExecuter> _placementExecuters = new();
 
+        private readonly Dictionary<ConstructionOrder, EntityComponents> _existingProjections = new();
+
         private readonly CameraSystem _cameraSystem;
         private readonly UIController _uIController;
         private readonly InputSystem _inputSystem;
+        private readonly UniversalSpawner _spawner;
+        private readonly BuildSystem _buildSystem;
 
-        private CannonProjection _spawnedProjection;
-        private ItemTypes _itemType;
+        private EntityComponents _spawnedProjectionComponents;
+        private ItemTypes _itemTipe;
 
         public PlaceObjectSystem(
             CameraSystem cameraSystem,
             UIController uIController,
             InputSystem inputSystem,
+            UniversalSpawner spawner,
+            BuildSystem buildSystem,
             params IPlacementExecuter[] placementExecuters)
         {
             _cameraSystem = cameraSystem;
             _uIController = uIController;
             _inputSystem = inputSystem;
+            _spawner = spawner;
+            _buildSystem = buildSystem;
 
             foreach (var executer in placementExecuters)
             {
@@ -45,74 +55,75 @@ namespace Assets.Scripts.Placement
             }
         }
 
-        public bool IsPlacingObject => _spawnedProjection != null;
-
-        private EventHandler<SpawnArguments> _cannonProjectionSpawnRequest;
-        private EventHandler<SpawnArguments> _cannonControllerSpawnRequest;
-
-        event EventHandler<SpawnArguments> ISpawnRequester<CannonProjection>.SpawnRequested
-        {
-            add => _cannonProjectionSpawnRequest += value;
-            remove => _cannonProjectionSpawnRequest -= value;
-        }
-
-        event EventHandler<SpawnArguments> ISpawnRequester<CannonController>.SpawnRequested
-        {
-            add => _cannonControllerSpawnRequest += value;
-            remove => _cannonControllerSpawnRequest -= value;
-        }
+        public bool IsPlacingObject { get; private set; }
 
         public event Action<ItemTypes> ObjectPlaced;
 
-        public void Place()
+        public async Task Place()
         {
             if (!IsPlacingObject)
                 return;
 
-            var position = GetCameraFacedPosition();
+            var hasPosition = _spawnedProjectionComponents.Get<IHasPosition>();
+            var hasRotation = _spawnedProjectionComponents.Get<IHasRotation>();
+            var constructionProjection = _spawnedProjectionComponents.Get<IConstructionProjection>();
 
-            _cannonControllerSpawnRequest?.Invoke(this, new SpawnArguments(_itemType, position, rotation: Quaternion.identity));
+            var order = await _buildSystem.OrderConstruction(hasPosition.Position, hasRotation.Rotation, constructionProjection.BuildCarriageOffset);
+
+            order.ConstructionStarted += OnConstructionStart;
+
+            _existingProjections[order] = _spawnedProjectionComponents;
+
+            _spawnedProjectionComponents = null;
+            IsPlacingObject = false;
+
+            ObjectPlaced?.Invoke(_itemTipe);
         }
 
-        public void ShowProjection(ItemTypes itemType)
+        public async void ShowProjection(ItemTypes itemType)
         {
-            _itemType = itemType;
-            var projection = _projections[itemType];
-
-            var position = GetCameraFacedPosition();
-
-            _cannonProjectionSpawnRequest?.Invoke(this, new SpawnArguments(projection, position, rotation: Quaternion.identity));
+            _itemTipe = itemType;
+            var projectionType = _projections[itemType];
 
             _uIController.ClearOpenWindow();
             _inputSystem.SwitchToLast();
-        }
 
-        public void SetSpawnedObject(CannonProjection cannonProjection)
-        {
-            _spawnedProjection = cannonProjection;
-        }
+            var position = GetCameraFacedPosition();
 
-        public void SetSpawnedObject(CannonController obj)
-        {
-            _spawnedProjection.Disable();
-            _spawnedProjection = null;
+            _spawnedProjectionComponents = await _spawner.SpawnAsync(projectionType, position, rotation: Quaternion.identity);
 
-            ObjectPlaced?.Invoke(_itemType);
+            IsPlacingObject = true;
         }
 
         public void Update()
         {
+            MoveCurrentProjection();
+        }
+
+        private void MoveCurrentProjection()
+        {
             if (!IsPlacingObject)
                 return;
 
             var position = GetCameraFacedPosition();
 
-            _spawnedProjection.Place(position, Quaternion.identity);
+            var placeable = _spawnedProjectionComponents.Get<IPlaceable>();
+            placeable.Place(position, Quaternion.identity);
         }
 
         private Vector3 GetCameraFacedPosition()
         {
             return _cameraSystem.MainCamera.GetFacedPosition(QueryTriggerInteraction.Ignore, LayerIds.BitMaskPlayer | LayerIds.BitMaskVendor);
+        }
+
+        private void OnConstructionStart(object sender, EventArgs e)
+        {
+            var order = (ConstructionOrder)sender;
+
+            var projection = _existingProjections[order];
+
+            var disabler = projection.Get<IDisabler>();
+            disabler.Disable();
         }
     }
 }
