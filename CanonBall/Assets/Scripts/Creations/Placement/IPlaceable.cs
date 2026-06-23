099894f6c8180713d8dda2f189471b4ff03926f9
Artem <nadulich2019@mail.ru>
2026-06-23 19:10:46 +0300

    added IPlaceable

---
+++ CanonBall/Assets/Scripts/Creations/Placement/IPlaceable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Creations.Placement
+{
+    public interface IPlaceable : IPositionable, IRotateable, IParentable
+    {
+        public void Place(Vector3 position, Quaternion rotation, Transform parent = null);
+    }
+}
