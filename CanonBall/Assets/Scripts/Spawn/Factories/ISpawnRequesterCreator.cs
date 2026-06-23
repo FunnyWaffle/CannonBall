51867f2b8cac6f35239e4e772402a89e2a645461
Artem <nadulich2019@mail.ru>
2026-06-23 19:31:05 +0300

    removed ISpawnRequesterCreator

--- CanonBall/Assets/Scripts/Spawn/Factories/ISpawnRequesterCreator.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Assets.Scripts.Spawn.Factories
-{
-    public interface ISpawnRequesterCreator<T>
-    {
-        public event Action<ISpawnRequester<T>> SpawnRequesterCreated;
-    }
-}
