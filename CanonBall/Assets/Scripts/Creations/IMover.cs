e816758ddec1cfa7587372e9e4f5991e5e13feaf
Artem <nadulich2019@mail.ru>
2026-06-23 18:49:10 +0300

    added IMover

---
+++ CanonBall/Assets/Scripts/Creations/IMover.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Creations
+{
+    public interface IMover : IComponent
+    {
+        public event Action Arrived;
+
+        public void SetDestination(Vector3 position);
+    }
+}
