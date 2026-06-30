c3a43bbc0e11b6baf3a17b412a1db008accb11bd
Artem <nadulich2019@mail.ru>
2026-06-30 21:59:30 +0300

    implement Updater into BuildCarriageFactory

--- CanonBall/Assets/Scripts/Spawn/Factories/BuildCarriageFactory.cs
+++ CanonBall/Assets/Scripts/Spawn/Factories/BuildCarriageFactory.cs
@@ -1,12 +1,23 @@
 ﻿using Assets.Scripts.Build.Carriage;
 using Assets.Scripts.Creations;
 using Assets.Scripts.Shop;
+using Assets.Scripts.Systems;
 using UnityEngine;
+using Zenject;
 
 namespace Assets.Scripts.Spawn.Factories
 {
     public class BuildCarriageFactory : IUniversalFactory
     {
+        private readonly DiContainer _diContainer;
+        private readonly Updater _updater;
+
+        public BuildCarriageFactory(DiContainer diContainer, Updater updater)
+        {
+            _diContainer = diContainer;
+            _updater = updater;
+        }
+
         public ItemTypes CreationType => ItemTypes.BuildCarriage;
 
         public EntityComponents Create(Transform prefab, Vector3 position, Quaternion rotation, Transform parent = null)
@@ -14,8 +25,12 @@ namespace Assets.Scripts.Spawn.Factories
             var obj = GameObject.Instantiate(prefab, position, rotation, parent);
             var view = obj.GetComponent<BuildCarriage>();
 
+            _diContainer.Inject(view);
+
             var components = new EntityComponents();
             components.Add(view);
+
+            _updater.SetUpdatable(view);
 
             return components;
         }
