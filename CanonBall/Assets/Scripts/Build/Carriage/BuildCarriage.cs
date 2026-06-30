3aa9adff7289ade331126bae568aefbf01f5649d
Artem <nadulich2019@mail.ru>
2026-06-30 21:57:08 +0300

    BuildCarriage script changes

--- CanonBall/Assets/Scripts/Build/Carriage/BuildCarriage.cs
+++ CanonBall/Assets/Scripts/Build/Carriage/BuildCarriage.cs
@@ -1,18 +1,28 @@
 ﻿using Assets.Scripts.Creations;
+using Assets.Scripts.Creations.Placement;
 using Assets.Scripts.Shop;
 using Assets.Scripts.Spawn;
+using Assets.Scripts.Systems;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+using Zenject;
 
 namespace Assets.Scripts.Build.Carriage
 {
-    public class BuildCarriage : MonoBehaviour, IPoolableObject, IDisabler, IEnableable, IMover, IUpdatable, IBuildCarriage
+    public class BuildCarriage : MonoBehaviour, IPoolableObject, IDisabler, IEnableable, IMover, IRotateable, IUpdatable, IBuildCarriage
     {
-        [SerializeField] private NavMeshAgent _navMeshAgent;
         [SerializeField] private Transform _trunkExit;
+        [SerializeField] private Vector3 _size;
 
-        private bool _pavedPath;
+        [Inject] private CustomPathFinder _customPathFinder;
+
+        private readonly List<Vector3> _path = new();
+
+        private readonly float _movementSpeed = 1f;
+
+        private bool _hasPath;
+        private int _currentCornerIndex = 0;
 
         public float TrunkOffset => Vector3.Distance(transform.position, _trunkExit.position);
         public Vector3 TrunkExit => _trunkExit.position;
@@ -21,9 +31,16 @@ namespace Assets.Scripts.Build.Carriage
         public event EventHandler<ItemTypes> Disabled;
         public event Action Arrived;
 
-        public void SetDestination(Vector3 position)
+        public async void SetDestination(Vector3 destination)
         {
-            _navMeshAgent.SetDestination(position);
+            var found = await _customPathFinder.TryFindAlloc(transform.position, _size / 2, destination, 0.2f, _path);
+
+            _hasPath = found;
+        }
+
+        public void Rotate(Quaternion rotation)
+        {
+
         }
 
         public void Disable()
@@ -39,19 +56,37 @@ namespace Assets.Scripts.Build.Carriage
 
         void IUpdatable.Update()
         {
-            _pavedPath = _navMeshAgent.hasPath;
-
-            if (!_pavedPath)
+            if (!_hasPath)
                 return;
 
-            var target = _navMeshAgent.pathEndPosition;
-            var distanceFromTarget = Vector3.Distance(target, transform.position);
+            Move();
 
-            if (distanceFromTarget <= _navMeshAgent.radius)
+            var target = _path[^1];
+            if (IsReached(target))
             {
-                _pavedPath = false;
+                _currentCornerIndex = 0;
+                _hasPath = false;
                 Arrived?.Invoke();
             }
+        }
+
+        private void Move()
+        {
+            var currentCorner = _path[_currentCornerIndex];
+            transform.position = Vector3.MoveTowards(transform.position, currentCorner, _movementSpeed * Time.deltaTime);
+
+            if (IsReached(currentCorner))
+                _currentCornerIndex++;
+        }
+
+        private bool IsReached(Vector3 position)
+        {
+            var distanceFromTarget = Vector3.Distance(position, transform.position);
+
+            if (distanceFromTarget <= _size.z / 2)
+                return true;
+
+            return false;
         }
     }
 }
