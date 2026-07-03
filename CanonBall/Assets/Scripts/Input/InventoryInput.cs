8b39991643a20b648fce0edbfd4c8e4df6fcba39
Artem <nadulich2019@mail.ru>
2026-07-03 18:56:58 +0300

    replaced UIInput by InventoryInput

---
+++ CanonBall/Assets/Scripts/Input/InventoryInput.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine.InputSystem;
+
+namespace Assets.Scripts.Input
+{
+    public class InventoryInput : IInputActionMap
+    {
+        private readonly InputSystem_Actions.InventoryActions _actions;
+
+        public InventoryInput(InputSystem_Actions.InventoryActions actions)
+        {
+            _actions = actions;
+
+            _actions.Close.performed += OnClosed;
+        }
+
+        public InputType Type => InputType.Inventory;
+        public bool CanBeInQueue => false;
+
+        public event Action ClosePefromed;
+
+        public void Disable()
+        {
+            _actions.Disable();
+        }
+
+        public void Enable()
+        {
+            _actions.Enable();
+        }
+
+        private void OnClosed(InputAction.CallbackContext context)
+        {
+            ClosePefromed?.Invoke();
+        }
+    }
+}
