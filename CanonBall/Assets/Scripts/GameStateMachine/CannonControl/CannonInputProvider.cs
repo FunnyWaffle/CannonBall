bca459d29592ee07d7a7c35d214ec325ddb7c2a9
Artem <nadulich2019@mail.ru>
2026-07-03 13:46:25 +0300

    added OnViewModePerformed method to CannonInputProvider

--- CanonBall/Assets/Scripts/GameStateMachine/CannonControl/CannonInputProvider.cs
+++ CanonBall/Assets/Scripts/GameStateMachine/CannonControl/CannonInputProvider.cs
@@ -1,4 +1,5 @@
-﻿using Assets.Scripts.Config;
+﻿using Assets.Scripts.Camera;
+using Assets.Scripts.Config;
 using Assets.Scripts.Creations;
 using Assets.Scripts.Input;
 using Assets.Scripts.Systems;
@@ -28,6 +29,7 @@ namespace Assets.Scripts.GameStateMachine.CannonControl
             _crosshairSystem = crosshairSystem;
 
             _input.ShootPerform += OnShoot;
+            _input.ViewModeActionPerformed += OnViewModePerformed;
         }
 
         public void Update()
@@ -48,6 +50,19 @@ namespace Assets.Scripts.GameStateMachine.CannonControl
         {
             if (_controllerContainer.TryGetController(out var controller))
                 controller.Shoot();
+        }
+
+        private void OnViewModePerformed(int index)
+        {
+            var viewType = index switch
+            {
+                0 => ViewType.FirstPerson,
+                1 => ViewType.ThirdPerson,
+                _ => throw new System.NotImplementedException("There's no view mode asign by index: " + index),
+            };
+
+            _cameraSystem.ChangeCameraViewType(viewType);
+            _crosshairSystem.SwitchCrosshairMode(viewType);
         }
     }
 }
