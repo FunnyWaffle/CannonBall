ee83b4dd3758350aa40852be1a55dfff2fa8fa44
Artem <nadulich2019@mail.ru>
2026-07-15 13:07:48 +0300

    added MainMenu script

---
+++ CanonBall/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private NewGameButton _newGameButton;
+        [SerializeField] private QuitGameButton _quitGameButton;
+
+        private void Start()
+        {
+            _newGameButton.Initialize();
+            _quitGameButton.Initialize();
+        }
+    }
+}
