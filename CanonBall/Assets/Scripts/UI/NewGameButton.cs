7ebc59909c1824aec62bcd4e730ebc23deb82486
Artem <nadulich2019@mail.ru>
2026-07-15 13:06:50 +0300

    added NewGameButton

---
+++ CanonBall/Assets/Scripts/UI/NewGameButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class NewGameButton : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+
+        public void Initialize()
+        {
+            _button.onClick.AddListener(OnNewGameButtonPressed);
+        }
+
+        private void OnNewGameButtonPressed()
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
