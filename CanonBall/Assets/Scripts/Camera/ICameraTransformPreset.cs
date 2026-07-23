06981ab7ff7cad5aea0072c25a00fef64b44e153
Artem <nadulich2019@mail.ru>
2026-07-23 19:35:30 +0300

    feat: added ICameraTransformPreset

---
+++ CanonBall/Assets/Scripts/Camera/ICameraTransformPreset.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Camera
+{
+    public interface ICameraTransformPreset
+    {
+        public Transform Pivot { get; }
+        public Transform Slot { get; }
+    }
+}
