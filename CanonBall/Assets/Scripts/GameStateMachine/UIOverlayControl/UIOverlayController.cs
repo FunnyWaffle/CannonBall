eee526d4a4a1aa6c717cd743485439e16fb0137c
Artem <nadulich2019@mail.ru>
2026-07-25 16:39:38 +0300

    feat: added UI Overlay

---
+++ CanonBall/Assets/Scripts/GameStateMachine/UIOverlayControl/UIOverlayController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Scripts.GameStateMachine.UIOverlayControl
+{
+    public class UIOverlayController
+    {
+        private readonly Dictionary<OverlayType, IUIOverlay> _overlays = new();
+
+        public UIOverlayController(params IUIOverlay[] overlays)
+        {
+
+            foreach (var overlay in overlays)
+            {
+                _overlays[overlay.Type] = overlay;
+            }
+        }
+
+        public void Show(OverlayType type)
+        {
+            var overlay = _overlays[type];
+            overlay.Show();
+        }
+
+        public void Hide(OverlayType type)
+        {
+            var overlay = _overlays[type];
+            overlay.Hide();
+        }
+    }
+
+    public interface IUIOverlay
+    {
+        public OverlayType Type { get; }
+
+        public void Show();
+        public void Hide();
+    }
+}
