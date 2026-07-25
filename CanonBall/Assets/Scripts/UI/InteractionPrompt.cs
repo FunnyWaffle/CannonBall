25abba60d6ce77e249508a8a5808f0c8d26013a1
Artem <nadulich2019@mail.ru>
2026-07-25 16:40:21 +0300

    feat: added InteractionPrompt

---
+++ CanonBall/Assets/Scripts/UI/InteractionPrompt.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Scripts.GameStateMachine.UIOverlayControl;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class InteractionPrompt : MonoBehaviour, IUIOverlay
+    {
+        public OverlayType Type => OverlayType.InteractionPrompt;
+
+        private void Start()
+        {
+            Hide();
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
