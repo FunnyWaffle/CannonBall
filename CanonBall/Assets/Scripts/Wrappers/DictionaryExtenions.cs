4d478beb46a478e3ebc21f2ce7368e453babc01b
Artem <nadulich2019@mail.ru>
2026-07-31 12:13:47 +0300

    feat: added DictionaryExtentions

---
+++ CanonBall/Assets/Scripts/Wrappers/DictionaryExtenions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Scripts.Wrappers
+{
+    public static class DictionaryExtenions
+    {
+        public static TValue GetOrCreate<TKey, TValue>(
+            this Dictionary<TKey, TValue> dictionary, TKey key)
+            where TValue : new()
+        {
+            if (!dictionary.TryGetValue(key, out var collection))
+            {
+                collection = new TValue();
+                dictionary[key] = collection;
+            }
+
+            return collection;
+        }
+
+        public static void Add<TKey, TCollection, TValue>(
+            this Dictionary<TKey, TCollection> dictionary,
+            TKey key, TValue value)
+            where TCollection : ICollection<TValue>, new()
+        {
+            if (!dictionary.TryGetValue(key, out var collection))
+            {
+                collection = new TCollection();
+                dictionary[key] = collection;
+            }
+
+            collection.Add(value);
+        }
+    }
+}
---
+++ CanonBall/Assets/Scripts/Wrappers/Vector3Extensions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Wrappers
+{
+    public static class Vector3Extensions
+    {
+        public static Vector3Int FloorToInt(this Vector3 vector, float cellSize)
+        {
+            var scaledVector = vector / cellSize;
+
+            var flatX = Mathf.FloorToInt(scaledVector.x);
+            var flatY = Mathf.FloorToInt(scaledVector.y);
+            var flatZ = Mathf.FloorToInt(scaledVector.z);
+
+            return new Vector3Int(flatX, flatY, flatZ);
+        }
+
+        public static Vector2Int FloorToIntXZ(this Vector3 vector, float cellSize)
+        {
+            var scaledVector = vector / cellSize;
+
+            var flatX = Mathf.FloorToInt(scaledVector.x);
+            var flatZ = Mathf.FloorToInt(scaledVector.z);
+
+            return new Vector2Int(flatX, flatZ);
+        }
+    }
+}
