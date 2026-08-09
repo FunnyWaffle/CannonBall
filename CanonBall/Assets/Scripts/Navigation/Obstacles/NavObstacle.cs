b755bb54dc67a9b95a3e44afacee2200a442fe15
Artem <nadulich2019@mail.ru>
2026-08-09 16:24:05 +0300

    feat: added NavObstacle

---
+++ CanonBall/Assets/Scripts/Navigation/Obstacles/NavObstacle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Navigation.Obstacles
+{
+    public class NavObstacle : MonoBehaviour
+    {
+        [SerializeField] private Bounds _bounds;
+        [SerializeField] private INavObstaclePart[] _parts;
+
+        public Vector3 Center => transform.position;
+        public Bounds Bounds => _bounds;
+    }
+}
