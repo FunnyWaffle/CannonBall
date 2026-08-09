4a2672c8caada3e3f09e359aa16ef01a01afd3fb
Artem <nadulich2019@mail.ru>
2026-08-09 16:24:36 +0300

    feat: added PathNode

---
+++ CanonBall/Assets/Scripts/Navigation/Path/PathNode.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Navigation.Path
+{
+    public class PathNode
+    {
+        public PathNode(
+            Vector3 position,
+            float costFromStart,
+            float distanceToTargetSq,
+            float totalCost,
+            PathNode previousPosition)
+        {
+            Position = position;
+            CostFromStart = costFromStart;
+            DistanceToTargetSq = distanceToTargetSq;
+            TotalCost = totalCost;
+            Parent = previousPosition;
+        }
+
+        public Vector3 Position { get; }
+
+        public float CostFromStart;
+        public float DistanceToTargetSq;
+        public float TotalCost;
+
+        public PathNode Parent;
+    }
+}
