7306c7452069012789207d0518bf1663fd250895
Artem <nadulich2019@mail.ru>
2026-08-09 16:26:46 +0300

    feat: added ListExtensions

---
+++ CanonBall/Assets/Scripts/Wrappers/ListExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Wrappers
+{
+    public static class ListExtensions
+    {
+        public static Vector3 GetClosest(this IEnumerable<Vector3> points, Vector3 position, out float distanceSqr)
+        {
+            var closest = Vector3.zero;
+            distanceSqr = float.MaxValue;
+
+            foreach (var point in points)
+            {
+                var currentDistance = Vector3.SqrMagnitude(point - position);
+
+                if (currentDistance < distanceSqr)
+                {
+                    currentDistance = distanceSqr;
+                    closest = point;
+                }
+            }
+
+            return closest;
+        }
+    }
+}
