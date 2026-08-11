d029552558935c7c7f3bdd842f107a6fec4caa8d
Artem <nadulich2019@mail.ru>
2026-08-11 20:51:46 +0300

    feat: added FloatExtensions
    added FloorToInt method with round to cell by _epsilon

---
+++ CanonBall/Assets/Scripts/Wrappers/FloatExtensions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Wrappers
+{
+    public static class FloatExtensions
+    {
+        private static readonly float _epsilon = 0.00001f;
+
+        public static int FloorToInt(
+            this float value,
+            float cellSize)
+        {
+            var nearestBoundary = Mathf.Round(value / cellSize) * cellSize;
+
+            if (Mathf.Abs(value - nearestBoundary) <= _epsilon)
+                value = nearestBoundary;
+
+            return Mathf.FloorToInt(value / cellSize);
+        }
+    }
+}
